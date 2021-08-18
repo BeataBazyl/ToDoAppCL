090c0784a573516c4a36578d4127ae49e067a87e
BeataBazyl <beata.m.bazyl@gmail.com>
2021-08-18 21:49:52 +0200

    adding components and api

---
+++ js/api/constants.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://todo-api.coderslab.pl/api";
+export const API_KEY = "6c1adbc3-04b7-4874-9cbf-a081c0af9680";
+
+// export const API_URL = "http://localhost/api";
+// export const API_KEY = "6c1adbc3-04b7-4874-9cbf-a081c0af9680";
